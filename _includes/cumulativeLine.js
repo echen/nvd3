075394430adf51b89b76739392a0ae92c8766e0c
Bob Monteverde <bobmonteverde@gmail.com>
2012-06-27 17:40:59 -0400

    making the comment span 1 line for easier reading

--- _includes/cumulativeLine.js
+++ _includes/cumulativeLine.js
@@ -3,7 +3,7 @@ d3.json('cumulativeLineData.json', function(data) {
   nv.addGraph(function() {
     var chart = nv.models.cumulativeLineChart()
                   .x(function(d) { return d[0] })
-                  .y(function(d) { return d[1]/100 }) //adjusting, 100% should be 1.00, not 100 as it is in the data
+                  .y(function(d) { return d[1]/100 }) //adjusting, 100% is 1.00, not 100 as it is in the data
                   .color(d3.scale.category10().range());
 
      chart.xAxis
