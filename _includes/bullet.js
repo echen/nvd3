841b0b10f05bc52240fb7d5ddd8791bba5379247
Bob Monteverde <bob@montev.com>
2012-06-30 03:25:00 -0400

    Added bullet example to gh-pages

---
+++ _includes/bullet.js
@@ -0,0 +1,21 @@
+
+nv.addGraph(function() {  
+  var chart = nv.models.bulletChart()
+                .width(400)
+                .height(80);
+
+  d3.select('#chart svg')
+      .datum(exampleData())
+    .transition().duration(1000)
+      .call(chart);
+
+  return chart;
+});
+
+
+
+function exampledata() {
+  return [
+    {"title":"Revenue","subtitle":"US$, in thousands","ranges":[150,225,300],"measures":[220],"markers":[250]}
+  ];
+}
