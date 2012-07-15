cb9e2d002e7d47d86f8f5139273446a344711021
Bob Monteverde <bob@montev.com>
2012-07-15 16:45:17 -0400

    Added historical stock example utilizing yahoo pipes

---
+++ ghpages/historicalStock.js
@@ -0,0 +1,113 @@
+
+d3.select('#stockForm').on('submit', chooseTicker);
+
+chooseTicker();
+
+
+function chooseTicker() {
+  if (d3.event) d3.event.preventDefault();
+
+  var ticker = d3.select('#ticker').property('value');
+  var startDate = d3.select('#startDate').property('value');
+  var endDate = d3.select('#endDate').property('value');
+  var frequency = d3.select('#frequency').property('value');
+
+  getHistoricalStockData(ticker, startDate, endDate, frequency);
+  getStockInfo(ticker);
+}
+
+
+function getStockInfo(symbol) {
+  d3.json('http://pipes.yahoo.com/pipes/pipe.run?_id=ctBz1YeB3BGxfaUVqWIyXQ&_render=json&StockNames=' + symbol, function(data) {
+    var info = data.value.items[0];
+
+   // nv.log(info);
+
+    d3.select('#stockName').text(info.StockName);
+    //d3.select('#stockSymbol').text(info.Symbol);
+    d3.select('#stockValue').text(info.StockValue);
+    d3.select('#stockChange')
+        .text(info.Change)
+        .style('color', parseFloat(info.Change) > 0 ? 'green' : 'red' );
+    d3.select('#stockChangePercent')
+        .text(d3.format('.2%')(parseInt(info.Change) / info.StockValue))
+        .style('color', parseFloat(info.Change) > 0 ? 'green' : 'red' );
+
+  });
+}
+
+
+function getHistoricalStockData(symbol, startDate, endDate, frequency) {
+  startDate = startDate || '-1 year';
+  endDate = endDate || 'now';
+  frequency = frequency || 'd';
+
+  d3.csv('http://pipes.yahoo.com/pipes/pipe.run?_id=e9668915a9ae04cb96c6f8c63279ad7f&_render=csv&enddate=' + endDate + '&startdate=' + startDate + '&frequency=' + frequency + '&ticker=' + symbol, function(data) {
+    //nv.log(data);
+
+    data = data.reverse(); //order data oldest to newest, left to right
+
+    var lineData = [
+        {
+          "key": "Volume",
+          "bar": true,
+          "values": data.map(function(d) {
+            return {
+              'x': Date.parse(d.Date),
+              'y': d.Volume / 1000  //TODO: figure out why y domain was maxing out at 9933800
+            }
+          })
+        },
+        {
+          "key": "Price",
+          "bar": false,
+          "values": data.map(function(d) {
+            return {
+              'x': Date.parse(d.Date),
+              'y': d.Close
+            }
+          })
+        }
+      ];
+
+    //nv.log(lineData);
+
+    //Having issue with tooltips when updating data
+    d3.selectAll('#chart svg *').remove();
+
+    nv.addGraph(function() {
+      var chart = nv.models.linePlusBarChart()
+          .margin({top: 30, right: 70, bottom: 50, left: 55})
+          .x(function(d,i) { return i })
+          .color(d3.scale.category10().range());
+
+      // Use if we are removing weekends/holidays
+      chart.xAxis
+          .tickPadding(7)
+          .tickFormat(function(d) {
+            var dx = lineData[0].values[d] && lineData[0].values[d].x || 0;
+            return d3.time.format('%x')(new Date(dx))
+          });
+
+      // Use to treat dates linear
+      //chart.xAxis.tickFormat(function(d) {
+        //return d3.time.format('%x')(new Date(d))
+      //});
+
+      chart.yAxis1
+          .tickFormat(function(d) { return d3.format(',f')(d) + 'K' });
+
+      chart.yAxis2
+          .tickFormat(function(d) { return '$' + d3.format(',.2f')(d) });
+
+      chart.bars.forceY([0]);
+
+      d3.select('#chart svg')
+          .datum(lineData)
+        .transition().duration(500).call(chart);
+
+      return chart;
+    });
+
+  });
+}
---
+++ ghpages/historicalStock.markdown
@@ -0,0 +1,49 @@
+---
+layout: ex
+title: Historical Stock Data using Yahoo Pipes
+---
+
+## Historical Stock Data using Yahoo Pipes
+
+<form id="stockForm" class="well form-inline">
+  <label for="ticker">Stock Ticker: </label>
+  <input type="text" id="ticker" class="input-small" name="ticker" value="GOOG"></input>
+
+  <label for="endDate">Start Date: </label>
+  <input type="text" id="startDate" class="input-small" name="startDate" value="-1 year"></input>
+
+  <label for="endDate">End Date: </label>
+  <input type="text" id="endDate" class="input-small" name="endDate" value="now"></input>
+
+  <label for="frequency">Frequency: </label>
+  <select id="frequency" class="input-small" name="frequency">
+    <option value="d">Daily</option>
+    <option value="w">Weekly</option>
+    <option value="m">Monthly</option>
+    <!--<option value="v">Dividends Only</option>-->
+  </select>
+  <button type="submit" id="getData" name="getData" class="btn btn-primary">Get Data</button>
+</form>
+
+<h1>
+  <span id="stockName"> </span>
+  <span id="stockValue"> </span>
+  <!--<span id="stockSymbol"> </span>-->
+  <small>
+    <span id="stockChange"> </span>
+    (<span id="stockChangePercent"> </span>)
+  </small>
+</h1>
+
+<div id="chart">
+  <svg style="height:500px"> </svg>
+</div>
+
+<script type="text/javascript" src="historicalStock.js"> </script>
+
+
+### Source Code
+
+{% highlight js linenos %}
+{% include historicalStock.js %}
+{% endhighlight %}
