9ad985ed4b05e4362a7e0cee2e379f7ce6b92f91
Bob Monteverde <bob@montev.com>
2012-07-18 02:08:58 -0400

    Fix historical stock example

--- ghpages/historicalStock.js
+++ ghpages/historicalStock.js
@@ -93,7 +93,7 @@ function getHistoricalStockData(symbol, startDate, endDate, frequency) {
     nv.addGraph(function() {
       var chart = nv.models.historicalStockChart()
           //.margin({top: 30, right: 70, bottom: 50, left: 55})
-          .x(function(d,i) { return i })
+          .x(function(d,i) { return d.dx })
           .color(d3.scale.category10().range());
 
       // Use if we are removing weekends/holidays
