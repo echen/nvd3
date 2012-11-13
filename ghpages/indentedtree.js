f60f99530d1a72c8d32c038d32d55761e5fa6ded
Philip Schatz <phil@cnx.org>
2012-11-13 14:30:50 -0500

    References to pages now use nvd3.github.com

--- ghpages/indentedtree.js
+++ ghpages/indentedtree.js
@@ -39,7 +39,7 @@ nv.addGraph(function() {
 function testData() {
   return [{
     key: 'NVD3',
-    url: 'http://novus.github.com/nvd3',
+    url: 'http://nvd3.github.com/nvd3',
     values: [
       {
         key: "Charts",
@@ -47,47 +47,47 @@ function testData() {
           {
             key: "Simple Line",
             type: "Historical",
-            url: "http://novus.github.com/nvd3/ghpages/line.html"
+            url: "http://nvd3.github.com/nvd3/ghpages/line.html"
           },
           {
             key: "Scatter / Bubble",
             type: "Snapshot",
-            url: "http://novus.github.com/nvd3/ghpages/scatter.html"
+            url: "http://nvd3.github.com/nvd3/ghpages/scatter.html"
           },
           {
             key: "Stacked / Stream / Expanded Area",
             type: "Historical",
-            url: "http://novus.github.com/nvd3/ghpages/stackedArea.html"
+            url: "http://nvd3.github.com/nvd3/ghpages/stackedArea.html"
           },
           {
             key: "Discrete Bar",
             type: "Snapshot",
-            url: "http://novus.github.com/nvd3/ghpages/discreteBar.html"
+            url: "http://nvd3.github.com/nvd3/ghpages/discreteBar.html"
           },
           {
             key: "Grouped / Stacked Multi-Bar",
             type: "Snapshot / Historical",
-            url: "http://novus.github.com/nvd3/ghpages/multiBar.html"
+            url: "http://nvd3.github.com/nvd3/ghpages/multiBar.html"
           },
           {
             key: "Horizontal Grouped Bar",
             type: "Snapshot",
-            url: "http://novus.github.com/nvd3/ghpages/multiBarHorizontal.html"
+            url: "http://nvd3.github.com/nvd3/ghpages/multiBarHorizontal.html"
           },
           {
             key: "Line and Bar Combo",
             type: "Historical",
-            url: "http://novus.github.com/nvd3/ghpages/linePlusBar.html"
+            url: "http://nvd3.github.com/nvd3/ghpages/linePlusBar.html"
           },
           {
             key: "Cumulative Line",
             type: "Historical",
-            url: "http://novus.github.com/nvd3/ghpages/cumulativeLine.html"
+            url: "http://nvd3.github.com/nvd3/ghpages/cumulativeLine.html"
           },
           {
             key: "Line with View Finder",
             type: "Historical",
-            url: "http://novus.github.com/nvd3/ghpages/lineWithFocus.html"
+            url: "http://nvd3.github.com/nvd3/ghpages/lineWithFocus.html"
           }
         ]
       },
@@ -97,7 +97,7 @@ function testData() {
           {
             key: "Legend",
             type: "Universal",
-            url: "http://novus.github.com/nvd3/examples/legend.html"
+            url: "http://nvd3.github.com/nvd3/examples/legend.html"
           }
         ]
       }
